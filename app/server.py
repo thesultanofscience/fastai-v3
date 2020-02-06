8c8e478bac6446d51a987ab04931648883ef407d
thesultanofscience <thesultanofscience@gmail.com>
2020-02-05 17:23:44 -0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1X6nFWagq2LEDuCI5rwfHX45UE-MYSQuL'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['Melanocytic nevi', 'Melanoma', 'Benign keratosis', 'Basal cell carcinoma', 'Actinic keratoses', 'Vascular lesions', 'Dermatofibroma']
 path = Path(__file__).parent
 
 app = Starlette()
